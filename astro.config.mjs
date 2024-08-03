834d59f43cb1656ae09ced33c706d85e10c42618
wanerkevin <wanerkevin@ordenador-portatil-de-waner.home>
2024-08-03 17:28:46 +0200

    Add vercel server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: "server",
-  adapter: node({
+  /*adapter: node({
     mode: "standalone",
-  }),
+  }),*/
+  adapter:vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
